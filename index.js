de497cce2caa3a06da28e455c95547e7631fd34c
Jolie Hobloss <jolieshobloss@gmail.com>
2020-12-07 13:34:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function findMatching(array,name){
 function fuzzyMatch(array,name){
   let result=array.filter(element=>
   {
-     return element.indexOf(name);
+     return element.indexOf(name)!==-1;
     
   });
   return result;
