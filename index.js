587b0ace596bebb3347f2c17e0cc5cf2ae3a40a8
Jolie Hobloss <jolieshobloss@gmail.com>
2020-12-07 13:20:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,13 +12,6 @@ function findMatching(array,name){
 
 /*const sinon = require( 'sinon' )
 
-
-    it('returns matching drivers if case does not match but letters do', function () {
-      const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
-
-      expect(findMatching(drivers, 'Bobby')).to.eql(['Bobby', 'bobby']);
-    });
-
     it('returns an empty array if there is no match', function () {
       const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
 
