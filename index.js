a84e0097cb99a3b1cf02c223ee80ff0b134b8ab1
Jolie Hobloss <jolieshobloss@gmail.com>
2020-12-07 13:50:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,37 +25,3 @@ function matchName(array,name){
   return result;
 }
 
-
- /* describe('matchName()', function () {
-    it('accesses the data structure to check if name matches', function   const drivers = [
-        {
-          name: 'Bobby',
-          hometown: 'Pittsburgh' },
-        {
-          name: 'Sammy',
-          hometown: 'New York' } ,
-        {
-          name: 'Sally',
-          hometown: 'Cleveland' },
-        {
-          name: 'Annette',
-          hometown: 'Los Angeles' },
-        {
-          name: 'Bobby',
-          hometown: 'Tampa Bay' }
-      ];
-
-      expect(matchName(drivers, 'Bobby')).to.eql([
-        {
-          name: 'Bobby',
-          hometown: 'Pittsburgh'
-        },
-        {
-          name: 'Bobby',
-          hometown: 'Tampa Bay'
-        }
-      ]);
-    });
-  });
-});
-*/
