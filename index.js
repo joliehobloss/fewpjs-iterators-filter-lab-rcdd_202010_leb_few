f7bd86ec7f550a111d3c31eae663a1581ae7363d
Jolie Hobloss <jolieshobloss@gmail.com>
2020-12-07 13:40:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,24 +21,4 @@ function fuzzyMatch(array,name){
 
 
 
- /*describe('fuzzyMatch()', function () {
-    const drivers = [];
-
-    beforeEach(function () {
-      drivers.length = 0;
-
-      const drivers=['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
-    });
-
-    it('returns a driver if beginning provided letters match', function () {
-      expect(fuzzyMatch(drivers, 'Sa')).to.have.members(['Sammy', 'Sarah', 'Sally']);
-    });
-
-    it('does not return drivers if only middle or ending letters match', function () {
-      expect(fuzzyMatch(drivers, 'y')).to.have.members([]);
-    });
-
-    it('does not return drivers if only middle or ending letters match', function () {
-      expect(fuzzyMatch(drivers, 'mm')).to.have.members([]);
-    });
-  });*/
+ /**/
