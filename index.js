cc12f76683c9cef4af8e6113efdc65db9d7a4942
Jolie Hobloss <jolieshobloss@gmail.com>
2020-12-07 13:50:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+
+function findMatching(array,name){
+  let result=array.filter(element=>
+  {
+    return element.toLowerCase()===name.toLowerCase();
+    
+  });
+  return result;
+}
+
+function fuzzyMatch(array,name){
+  let result=array.filter(element=>
+  {
+     return element.substring(0, 2).indexOf(name)!==-1;
+    
+  });
+  return result;
+}
+
+
+function matchName(array,name){
+  let result=array.filter(element=>element.name===name);
+  return result;
+}
+
